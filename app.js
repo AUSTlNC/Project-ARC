9eaf8252a9222243a6f883c7de5e17bb51969128
AUSTlNC <jcai44@emory.edu>
2021-10-12 23:33:02 -0400

    merging google login

--- app.js
+++ app.js
@@ -1,25 +1,40 @@
-// var createError = require('http-errors');
 var express = require('express');
-
-// var path = require('path');
-// var cookieParser = require('cookie-parser');
-// var logger = require('morgan')
+var dotenv = require('dotenv')
 var passport = require('passport');
-// const { appendFile } = require('fs');
+const connectDB = require('./config/db')
+const session = require('express-session')
+
+//Load config
+dotenv.config({path: './config/config.env'})
+
+//Passport config
+require('./config/passport')(passport)
+
+connectDB()
 
 var app = express();
 
+//Sessions
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    //cookie: {secire: true}
+    //store in db database
+}))
+
+
+//Passport middleware
 app.use(passport.initialize());
+app.use(passport.session());
 
-require('./authenticate');
+//Routes
+app.use('/auth', require('./routes/auth'))
 
-app.get('/google', passport.authenticate('google', { scope: ['profile', 'email']}));
+const PORT = process.env.PORT || 5000
 
-app.get('/google/callback', passport.authenticate('google', { failureRedirect: '/login'}), (req, res) => {
-    // res.redirect('/dashboard');
-    res.end('Logged in!');
-})
 
-app.listen(3001, ()=>{
-    console.log("Server running on 3001");
-});
+app.listen(
+    PORT,
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
---
+++ routes/auth.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const passport = require('passport')
+const router = express.Router()
+
+router.get('/google', passport.authenticate('google', {scope: ['openid', 'profile', 'email']}))
+
+router.get(
+    '/google/callback', 
+    passport.authenticate('google', { failureRedirect:'/'}), 
+    (req, res) => {    
+        res.redirect('/dashboard')
+    }
+)
+
+module.exports = router
