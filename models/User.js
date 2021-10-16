4245cd1bebc685e8aa8c82739020b233e4ccf18f
Chloe Luo <74508347+bluekemono@users.noreply.github.com>
2021-10-16 15:49:34 -0400

    Squashed commit of the following:
    commit b56d38eb7e16de286067cdd1e8156c9f80cc0955
    Author: AUSTlNC <jcai44@emory.edu>
    Date:   Sat Oct 16 15:45:56 2021 -0400
    
        edit server.js
    
    commit 31a5e4282e294556e41302fb46c069e9c271a43f
    Author: AUSTlNC <jcai44@emory.edu>
    Date:   Wed Oct 13 15:35:11 2021 -0400
    
        get google
    
    commit ce8a5fc4fe5c4cdbb7eec36de98bf088c8017c86
    Author: AUSTlNC <jcai44@emory.edu>
    Date:   Wed Oct 13 11:33:55 2021 -0400
    
        add cors
    
    commit 7d004486b3279b4902ef0e5fc3be98dd51c4594a
    Author: AUSTlNC <jcai44@emory.edu>
    Date:   Tue Oct 12 23:52:55 2021 -0400
    
        merge app and server
    
    commit 6040a021c1767da3235aafca425646423a4cfe2d
    Author: AUSTlNC <jcai44@emory.edu>
    Date:   Tue Oct 12 23:35:15 2021 -0400
    
        delete flash
    
    commit 9eaf8252a9222243a6f883c7de5e17bb51969128
    Author: AUSTlNC <jcai44@emory.edu>
    Date:   Tue Oct 12 23:33:02 2021 -0400
    
        merging google login
    
    commit 770d5307ceeb557bb80b2ffce17f888953bde724
    Author: AUSTlNC <jcai44@emory.edu>
    Date:   Tue Oct 12 14:28:45 2021 -0400
    
        add post

--- models/User.js
+++ models/User.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose")
 
 const UserSchema = new mongoose.Schema({
     username: {type: String, required: true, unique: true},
-    password:{type: String, required: true}
+    password: {type: String, required: true}
 }, {collection: "users"})
 
-const UserModel = mongoose.model("UserSchema", UserSchema)
+const UserModel = mongoose.model('UserSchema', UserSchema)
 module.exports = UserModel
--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const mongoose = require('mongoose')
 const User = require('./models/User')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const postRouter = require('./routes/posting')
+var dotenv = require('dotenv')
+var passport = require('passport')
+const session = require('express-session')
+const cors = require('cors')
+dotenv.config({path: './config/config.env'})
 
 const JWT_SECRET = 'asdfjaoiwer987q293rhajksdhfyasdfkh*&^*%'
 
@@ -13,9 +19,39 @@ mongoose.connect("mongodb+srv://austin:caijh20000609@arc-main.ih4xb.mongodb.net/
     useUnifiedTopology: true
 })
 
+
+//Load config
+dotenv.config({path: './config/config.env'})
+
+//Passport config
+require('./config/passport')(passport)
+
+
 const app = express()
+
+// use cors before all route definitions
+app.use(cors());
+
+//Sessions
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    //cookie: {secire: true}
+    //store in db database
+}))
+
+
 app.use('/', express.static(path.join(__dirname, 'static')))
 app.use(bodyParser.json())
+app.use('/posts', postRouter)
+
+//Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Routes
+app.use('/auth', require('./routes/auth'))
 
 
 app.post('/api/change-password', async (req, res) => {
@@ -50,16 +86,15 @@ app.post('/api/login', async (req, res) => {
         return res.json({status: 'ok', data: 'GOOD'})
     }
 
-    res.json({status: 'error', data: 'Invalid username/password'})
+    return res.json({status: 'error', error: 'Invalid username/password'})
 })
-
-
 
 app.post('/api/register', async (req, res) => {
 	console.log(req.body)
     const {username,  password: plainTextPassword } = req.body
     
     if(!username || typeof username !== 'string') {
+        res.status()
         return res.json({status: 'error', error: 'Invalid username'})
     }
     if(!plainTextPassword || typeof plainTextPassword !== 'string') {
@@ -77,6 +112,7 @@ app.post('/api/register', async (req, res) => {
     try {
         const response = await User.create({username, password})
         console.log('User created successfully: ', response)
+
     } catch(error) {
         console.log(JSON.stringify(error))
         if (error.code === 11000) {
@@ -87,11 +123,13 @@ app.post('/api/register', async (req, res) => {
     }
     
     // console.log(await bcrypt.hash(password, 12))
-	
-    
-    res.json({ status: 'ok' })
+    return res.json({status: 'ok', data: 'GOOD'})
 })
 
-app.listen(9999, () => {
-	console.log('Server up at 9999')
-})
+const PORT = process.env.PORT || 9999
+
+
+app.listen(
+    PORT,
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
