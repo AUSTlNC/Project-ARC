4bb95c0e1cec4355c73796232437edb400f88399
AUSTlNC <jcai44@emory.edu>
2021-10-23 16:06:40 -0400

    upload image

--- models/Post.js
+++ models/Post.js
@@ -3,10 +3,13 @@ const mongoose = require("mongoose")
 
 const PostSchema = new mongoose.Schema({
     title: {type: String, required: true},
+    userinfo: {type: mongoose.Schema.Types.ObjectId, required: true, ref: "UserSchema"},
     description:{type: String, required: true},
     artType:{type: String, required: true},
     photoURL:{type: String, required: true},
-    createdAt:{type: Date, default: Date.now}
+    createdAt:{type: Date, default: Date.now},
+    image:{type: Buffer, required: true},
+    imageType:{type: String, required: true}
 }, {collection: "posts"})
 
 const PostModel = mongoose.model("PostSchema", PostSchema)
