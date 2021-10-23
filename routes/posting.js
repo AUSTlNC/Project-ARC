b7e2db20c93758c8a71ead6f7e844c48347b2262
AUSTlNC <jcai44@emory.edu>
2021-10-23 16:06:01 -0400

    upload image

--- routes/posting.js
+++ routes/posting.js
@@ -1,12 +1,24 @@
 const express = require('express')
 const router = express.Router()
 const Post = require('./../models/Post')
-router.get('/')
+const User = require('./../models/User')
+const validImageTypes = ['image/jpeg', 'image/png']
+
+// get all posts
+router.get('/', async (req, res) => {
+
+} )
 
 router.post('/', async (req, res) => {
-    const { title, description, artType, photoURL } = req.body
+    const post = new Post({
+        title: req.body.title, 
+        description: req.body.description, 
+        artType: req.body.artType,
+        photoURL: req.body.photoURL
+    })
+    saveImage(post, req.body.files)
     try {
-        const response = await Post.create({title, description, artType, photoURL})
+        const response = await Post.create(post)
         console.log('Post created successfully: ', response)
     } catch(error) {
         console.log(JSON.stringify(error))
@@ -14,5 +26,14 @@ router.post('/', async (req, res) => {
     }
 })
 
+
+function saveImage(post, imageEncoded) {
+    if (imageEncoded == null) return
+    const img = JSON.parse(imageEncoded)
+    if (img != null && validImageTypes.includes(img.type)) {
+        post.image = new Buffer.from(img.data, 'base64')
+        post.imageType = img.type
+    }
+}
 
 module.exports = router
