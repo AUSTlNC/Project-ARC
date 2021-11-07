02701c036d039b00cc759303ad0b36d22f75629e
leosheng <shengjiasheng2000@163.com>
2021-11-07 17:40:33 -0500

    add comment

---
+++ routes/commenting.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const router = express.Router()
+const Comment = require('./../models/Comment')
+router.get('/')
+
+// badwords filter used: https://www.npmjs.com/package/bad-words
+var Filter = require('bad-words'),
+    swearfilter = new Filter();
+
+// spamfilter used : https://www.npmjs.com/package/spam-filter
+const spamfilter = require('spam-filter')('naiveBayes')
+
+router.post('/', async (req, res) => {
+    const { userId, postId, comment} = req.body
+    if(swearfilter.isProfane(comment)) {
+        return res.json({status: 'error', error: 'Profanity detected'})
+    }
+    if(spamfilter.isSpam(comment)){
+        return res.json({status: 'error', error: 'Spam detected'})
+    }
+
+    if(comment.length < 10){
+        return res.json({status: 'error', error: 'Response too short'})
+    }
+
+    try {
+        const response = await Comment.create({userId, postId, comment})
+        console.log('Comment created successfully: ')
+    } catch(error) {
+        console.log(JSON.stringify(error))
+        throw error
+    }
+    console.log(comment)
+    return res.json({status: 'ok'})
+})
+/*
+async function searchComment(req, res){
+    const {post_id} = req.body
+    var comments = Comment.find({postId: post_id})
+    return res.json(comments)
+}
+*/
+
+
+module.exports = router
